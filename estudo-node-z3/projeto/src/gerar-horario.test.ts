a0fc6882c2c0fb1cd2d1c2253d1dab3723b151f3
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-10 00:00:35 -0400

    wow

---
+++ estudo-node-z3/projeto/src/gerar-horario.test.ts
@@ -0,0 +1,73 @@
+import { expect, test } from "vitest";
+import { DiaSemanaIso, GeradorHorario, gerarTempos } from "./gerar-horario";
+
+test("adds 1 + 2 to equal 3", async () => {
+  const gerador = new GeradorHorario();
+
+  const horario = gerador.gerarHorario({
+    diaInicio: DiaSemanaIso.SEGUNDA,
+    diaFim: DiaSemanaIso.SEXTA,
+    //
+
+    tempos: [
+      //
+      ...gerarTempos("07:30", 50, 3),
+      ...gerarTempos("10:20", 50, 2),
+      ...gerarTempos("13:00", 50, 3),
+      ...gerarTempos("15:50", 50, 2),
+    ],
+
+    //
+    professores: [
+      {
+        id: 1,
+        nome: "Danilo",
+        disponibilidades: [
+          {
+            diaSemanaIso: DiaSemanaIso.SEGUNDA,
+            inicio: "07:30",
+            fim: "12:00",
+          },
+          {
+            diaSemanaIso: DiaSemanaIso.SEGUNDA,
+            inicio: "13:30",
+            fim: "17:30",
+          },
+        ],
+      },
+    ],
+
+    turmas: [
+      {
+        id: 1,
+        nome: "1A INF",
+
+        disponibilidades: [
+          {
+            diaSemanaIso: DiaSemanaIso.SEGUNDA,
+            inicio: "13:00",
+            fim: "17:30",
+          },
+        ],
+
+        diarios: [
+          {
+            id: 1,
+
+            disciplina: {
+              nome: "Redes",
+            },
+
+            quantidadeMaximaSemana: 2,
+
+            professor: {
+              id: 1,
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  await expect(horario).resolves.toBeDefined();
+});
