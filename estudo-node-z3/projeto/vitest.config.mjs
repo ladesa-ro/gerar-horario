23c19fb754cd13ccdc7d64a66e146098b37f0866
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-10 07:31:09 -0400

    change: estudo-node-z3: vitest: add text and json reporter

--- estudo-node-z3/projeto/vitest.config.mjs
+++ estudo-node-z3/projeto/vitest.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['json-summary'],
+      reporter: ['text', 'json-summary', 'json'],
       reportOnFailure: true,
     }
   }
