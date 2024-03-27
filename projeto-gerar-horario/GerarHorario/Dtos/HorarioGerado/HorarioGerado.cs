1fffc0db3ec80fbe6b564c1283aa6a20125a882d
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-27 13:25:59 -0400

    feat: Main: call GerarHorario

--- projeto-gerar-horario/GerarHorario/Dtos/HorarioGerado/HorarioGerado.cs
+++ projeto-gerar-horario/GerarHorario/Dtos/HorarioGerado/HorarioGerado.cs
@@ -7,6 +7,10 @@ public class HorarioGerado
 
     public override string ToString()
     {
-        return "HorarioGerado {  }";
+        return $@"HorarioGerado {{
+    Aulas ({Aulas.Length}) [
+        
+    ]
+}}";
     }
 }
--- projeto-gerar-horario/GerarHorario/Main.cs
+++ projeto-gerar-horario/GerarHorario/Main.cs
@@ -8,29 +8,29 @@ public class Main
 
     public bool Retorno()
     {
-        // var gerarTodosOsHorarios = false;
+        var gerarTodosOsHorarios = false;
 
         // ====================================================
 
-        // var gerarHorarioOptions = new GerarHorarioOptions((int)DiaSemanaIso.SEGUNDA,(int)DiaSemanaIso.SABADO, );
+        var gerarHorarioOptions = new GerarHorarioOptions((int)DiaSemanaIso.SEGUNDA, (int)DiaSemanaIso.SABADO, [], [], []);
 
         // ====================================================
-        // var horarioGeradoEnumerator = Gerador.GerarHorario(gerarHorarioOptions);
+        var horarioGeradoEnumerator = Gerador.GerarHorario(gerarHorarioOptions);
         // ====================================================
 
-        // var melhorHorario = horarioGeradoEnumerator.First();
-        // Console.WriteLine($"Melhor horário gerado: {melhorHorario}");
+        var melhorHorario = horarioGeradoEnumerator.First();
+        Console.WriteLine($"Melhor horário gerado: {melhorHorario}");
 
 
         // ====================================================
 
-        // if (gerarTodosOsHorarios)
-        // {
-        // foreach (var horarioGerado in horarioGeradoEnumerator)
-        // {
-        // Console.WriteLine($"Horário Gerado: {horarioGerado}");
-        // }
-        // }
+        if (gerarTodosOsHorarios)
+        {
+            foreach (var horarioGerado in horarioGeradoEnumerator)
+            {
+                Console.WriteLine($"Horário Gerado: {horarioGerado}");
+            }
+        }
 
         // ====================================================
 
