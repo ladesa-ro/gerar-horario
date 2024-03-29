a3f4b5ed3471202f16ba20994737cc40565d2e77
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-29 15:59:02 -0400

    feat: GerarHorario-Service: consumer

--- projeto-gerar-horario/Microsservicos/GerarHorario-Service/Program.cs
+++ projeto-gerar-horario/Microsservicos/GerarHorario-Service/Program.cs
@@ -1,50 +1,86 @@
 using System.Text;
+using System.Text.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using Sisgea.GerarHorario.Core;
+using Sisgea.GerarHorario.Core.Dtos.Configuracoes;
+using Sisgea.GerarHorario.Core.Dtos.HorarioGerado;
 
 var threadApi = new Thread(() =>
 {
-  var builder = WebApplication.CreateBuilder(args);
-  var app = builder.Build();
+    var builder = WebApplication.CreateBuilder(args);
+    var app = builder.Build();
 
-  app.MapGet("/", () => "Hello World!");
+    app.MapGet("/", () => "Hello World!");
 
-  app.Run();
+    app.Run();
 });
 
 
 var threadFila = new Thread(() =>
 {
-  var factory = new ConnectionFactory { HostName = "localhost" };
-  factory.UserName = "admin";
-  factory.Password = "admin";
-  using var connection = factory.CreateConnection();
+    var factory = new ConnectionFactory { HostName = "localhost" };
+    factory.UserName = "admin";
+    factory.Password = "admin";
+    using var connection = factory.CreateConnection();
 
-  // using var channel = connection.CreateModel();
-  // channel.QueueDeclare(queue: "pedido_gerar_horario",
-  //                      durable: true,
-  //                      exclusive: false,
-  //                      autoDelete: false,
-  //                      arguments: null);
+    using var channel = connection.CreateModel();
+    channel.QueueDeclare(
+        queue: "gerar_horario",
+        durable: true,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
 
-  // Console.WriteLine(" [*] Waiting for messages.");
+    channel.QueueDeclare(
+        queue: "horario_gerado",
+        durable: true,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
 
-  // var consumer = new EventingBasicConsumer(channel);
-  // consumer.Received += (model, ea) =>
-  // {
-  //     var body = ea.Body.ToArray();
-  //     var message = Encoding.UTF8.GetString(body);
-  //     Console.WriteLine($" [x] Received {message}");
-  // };
+    void publicarRespostaGerarHorario(HorarioGerado? horarioGerado = null)
+    {
+        // TODO: message = JSON(horarioGerado)
+        const string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
 
-  // channel.BasicConsume(queue: "hello",
-  //                      autoAck: true,
-  //                      consumer: consumer);
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: "horario_gerado",
+                             basicProperties: null,
+                             body: body);
 
-  Console.WriteLine(" Press [enter] to exit.");
-  Console.ReadLine();
+        Console.WriteLine($" [x] Sent {message}");
+    }
+
+    Console.WriteLine(" [*] Waiting for messages.");
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($" [x] Received {message}");
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        GerarHorarioOptions? gerarHorarioOptions = JsonSerializer.Deserialize<GerarHorarioOptions>(message, options);
+        Console.WriteLine(gerarHorarioOptions);
+
+        publicarRespostaGerarHorario();
+    };
+
+    channel.BasicConsume(queue: "gerar_horario",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.ReadLine();
 });
 
-threadApi.Start();
 threadFila.Start();
+threadApi.Start();
