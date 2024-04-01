1346f5ab358e4519d121d41e91224300227085d3
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2024-03-31 20:08:42 -0400

    chore: adapt program.cs to use middleware notation

--- projeto-gerar-horario/Microsservicos/GerarHorario-Service/Program.cs
+++ projeto-gerar-horario/Microsservicos/GerarHorario-Service/Program.cs
@@ -1,86 +1,11 @@
-using System.Text;
-using System.Text.Json;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using Sisgea.GerarHorario.Core;
-using Sisgea.GerarHorario.Core.Dtos.Configuracoes;
-using Sisgea.GerarHorario.Core.Dtos.HorarioGerado;
+using GerarHorario_Service.Extensions;
 
-var threadApi = new Thread(() =>
-{
-    var builder = WebApplication.CreateBuilder(args);
-    var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-    app.MapGet("/", () => "Hello World!");
-
-    app.Run();
-});
+app.MapGet("up", () => "up");
 
 
-var threadFila = new Thread(() =>
-{
-    var factory = new ConnectionFactory { HostName = "localhost" };
-    factory.UserName = "admin";
-    factory.Password = "admin";
-    using var connection = factory.CreateConnection();
+app.UseQueueListener();
 
-    using var channel = connection.CreateModel();
-    channel.QueueDeclare(
-        queue: "gerar_horario",
-        durable: true,
-        exclusive: false,
-        autoDelete: false,
-        arguments: null
-    );
-
-    channel.QueueDeclare(
-        queue: "horario_gerado",
-        durable: true,
-        exclusive: false,
-        autoDelete: false,
-        arguments: null
-    );
-
-    void publicarRespostaGerarHorario(HorarioGerado? horarioGerado = null)
-    {
-        // TODO: message = JSON(horarioGerado)
-        const string message = "Hello World!";
-        var body = Encoding.UTF8.GetBytes(message);
-
-        channel.BasicPublish(exchange: string.Empty,
-                             routingKey: "horario_gerado",
-                             basicProperties: null,
-                             body: body);
-
-        Console.WriteLine($" [x] Sent {message}");
-    }
-
-    Console.WriteLine(" [*] Waiting for messages.");
-
-    var consumer = new EventingBasicConsumer(channel);
-    consumer.Received += (model, ea) =>
-    {
-        var body = ea.Body.ToArray();
-        var message = Encoding.UTF8.GetString(body);
-        Console.WriteLine($" [x] Received {message}");
-
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true
-        };
-
-        GerarHorarioOptions? gerarHorarioOptions = JsonSerializer.Deserialize<GerarHorarioOptions>(message, options);
-        Console.WriteLine(gerarHorarioOptions);
-
-        publicarRespostaGerarHorario();
-    };
-
-    channel.BasicConsume(queue: "gerar_horario",
-                         autoAck: true,
-                         consumer: consumer);
-
-    Console.ReadLine();
-});
-
-threadFila.Start();
-threadApi.Start();
+app.Run();
