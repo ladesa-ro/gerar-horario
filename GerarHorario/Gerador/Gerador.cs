9c8e4f2ec7c4780f41f84b20956485fb0021f382
Uriel Procópio <procopiouriel928@gmail.com>
2024-05-16 14:51:58 -0400

    feat: restriction implementation HorarioAlmocoTurma

--- GerarHorario/Gerador/Gerador.cs
+++ GerarHorario/Gerador/Gerador.cs
@@ -26,11 +26,11 @@ public class Gerador
 
         // ======================================
         // RESTRIÇÃO: Professor: não ter mais de uma aula ativa ao mesmo tempo.
-        Restricoes.AplicarLimiteDeNoMaximoUmDiarioAtivoPorProfessorEmUmHorario(contexto);
+        //Restricoes.AplicarLimiteDeNoMaximoUmDiarioAtivoPorProfessorEmUmHorario(contexto);
 
         // ======================================
         // RESTRIÇÃO: Diário: respeitar limite de quantidade máxima na semana.
-        Restricoes.AplicarLimiteDeDiarioNaSemana(contexto);
+        //Restricoes.AplicarLimiteDeDiarioNaSemana(contexto);
 
         // ======================================
         //RESTRIÇÃO: Mínimo de 1h30 de almoço para o professor.
@@ -42,7 +42,7 @@ public class Gerador
 
         // ======================================
         //RESTRIÇÃO: O professor não pode trabalhar 3 turnos e o professor não pode trabalhar de manhã e à noite.
-        Restricoes.ProfessorNaoPodeTrabalharEmTresTurnosDiferentes(contexto);
+        //Restricoes.ProfessorNaoPodeTrabalharEmTresTurnosDiferentes(contexto);
         // ====================================================================
 
 
